7a3765424910a06ca7bee4c937131bb5603a739e
kunal bodke <kunal.bodke@cwdin.com>
2023-11-30 11:22:39 +0530

    changed yml1

--- build.sh
+++ build.sh
@@ -8,17 +8,18 @@ MODULE_PATH="./go.mod"
 
 echo "Building $BUILD_NUMBER" 
 
-# log docker into eks using aws cli
-$(aws ecr get-login --no-include-email --region $REGION)
-# build a docker image with a unique build number
+# Log in to Amazon ECR
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_URL
+
+# Build a Docker image with a unique build number
 docker build -t $ECR_URL:$BUILD_NUMBER \
   --build-arg MODULE_PATH="$MODULE_PATH" \
   --build-arg VERSION="$BUILD_NUMBER" \
   .
 
-# push to ecr
+# Push to ECR
 docker push $ECR_URL:$BUILD_NUMBER
 
-#tag for "latest" and also push that
+# Tag for "latest" and also push that
 docker tag $ECR_URL:$BUILD_NUMBER $ECR_URL:latest
 docker push $ECR_URL:latest
